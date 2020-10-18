5a8fc4dddc76c3eb26938439b4f70833f64d11bc
ikasuu <71949537+ikasuu@users.noreply.github.com>
2020-10-17 17:42:50 -0700

    Updates

---
+++ precache-manifest.7bd85d7b0a1b3c45a00003b6abdf2bc6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee10bc29447f27a256096e73e0c3f289",
+    "url": "/GrandisLibrary/index.html"
+  },
+  {
+    "revision": "1a4de42909b04d84823b",
+    "url": "/GrandisLibrary/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "919bf5c181c76b25c3ea",
+    "url": "/GrandisLibrary/static/css/main.f931a45a.chunk.css"
+  },
+  {
+    "revision": "1a4de42909b04d84823b",
+    "url": "/GrandisLibrary/static/js/2.5f158072.chunk.js"
+  },
+  {
+    "revision": "2a4d9b0d8134dea1e9e34f680252bab7",
+    "url": "/GrandisLibrary/static/js/2.5f158072.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "919bf5c181c76b25c3ea",
+    "url": "/GrandisLibrary/static/js/main.da8f6ff3.chunk.js"
+  },
+  {
+    "revision": "8b963e34e904391cc1a5",
+    "url": "/GrandisLibrary/static/js/runtime-main.bfc0f0c2.js"
+  },
+  {
+    "revision": "d9fc4c2294ac7fe61d81a9bb7c3183da",
+    "url": "/GrandisLibrary/static/media/logo.d9fc4c22.webp"
+  }
+]);
