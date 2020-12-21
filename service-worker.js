0fb6263818d7dc802d704584106d5e7f1784f35d
ikasuu <71949537+ikasuu@users.noreply.github.com>
2020-12-20 18:58:01 -0800

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/GrandisLibrary/precache-manifest.882687a8d0bb8c254a11ee824ca60fe3.js"
+  "/GrandisLibrary/precache-manifest.e1427116bea6e59c926da650359f23d9.js"
 );
 
 self.addEventListener('message', (event) => {
--- static/js/15.a5d272d5.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonpgrandislibrary=this.webpackJsonpgrandislibrary||[]).push([[15],{186:function(n,r,a){"use strict";a.r(r);var i=a(0),t=a.n(i);r.default=function(){return t.a.createElement("div",null)}}}]);
-//# sourceMappingURL=15.a5d272d5.chunk.js.map
