2bcff77ba2054d2a5e075c326f50e8ad101ada37
ikasuu <71949537+ikasuu@users.noreply.github.com>
2021-02-18 23:03:40 -0800

    Updates

---
+++ precache-manifest.2c7975a5ff0b77b360e1c300ac142a08.js
@@ -0,0 +1,122 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21610263fd2cb2329f14d64369d43410",
+    "url": "/GrandisLibrary/index.html"
+  },
+  {
+    "revision": "2df77c464284d54f26a6",
+    "url": "/GrandisLibrary/static/css/0.732f59b7.chunk.css"
+  },
+  {
+    "revision": "408a4224e9bc23bd3456",
+    "url": "/GrandisLibrary/static/css/10.6f468a67.chunk.css"
+  },
+  {
+    "revision": "20fcb95ae665cd19ac79",
+    "url": "/GrandisLibrary/static/css/11.d63f724a.chunk.css"
+  },
+  {
+    "revision": "296cbcce41ee354959d5",
+    "url": "/GrandisLibrary/static/css/12.8520875b.chunk.css"
+  },
+  {
+    "revision": "4769085f5a1e28c38ba0",
+    "url": "/GrandisLibrary/static/css/4.2a86faee.chunk.css"
+  },
+  {
+    "revision": "ea10db75c85af0487a0f",
+    "url": "/GrandisLibrary/static/css/6.d63f724a.chunk.css"
+  },
+  {
+    "revision": "95c8c90e7e69b797020e",
+    "url": "/GrandisLibrary/static/css/7.5158de9f.chunk.css"
+  },
+  {
+    "revision": "42317971cba03d7c99fb",
+    "url": "/GrandisLibrary/static/css/main.29ec93e6.chunk.css"
+  },
+  {
+    "revision": "2df77c464284d54f26a6",
+    "url": "/GrandisLibrary/static/js/0.bf5d8656.chunk.js"
+  },
+  {
+    "revision": "22e0c182fd7d233289ae",
+    "url": "/GrandisLibrary/static/js/1.a5d110bf.chunk.js"
+  },
+  {
+    "revision": "742d91f9305ee4f84184373d3cbcb934",
+    "url": "/GrandisLibrary/static/js/1.a5d110bf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "408a4224e9bc23bd3456",
+    "url": "/GrandisLibrary/static/js/10.1e8c8e0e.chunk.js"
+  },
+  {
+    "revision": "20fcb95ae665cd19ac79",
+    "url": "/GrandisLibrary/static/js/11.c3f2d4a0.chunk.js"
+  },
+  {
+    "revision": "296cbcce41ee354959d5",
+    "url": "/GrandisLibrary/static/js/12.d372dc46.chunk.js"
+  },
+  {
+    "revision": "636084f9ff5d83dd4017",
+    "url": "/GrandisLibrary/static/js/13.c6e2ec1d.chunk.js"
+  },
+  {
+    "revision": "4d4df8d8026c3370b4fe",
+    "url": "/GrandisLibrary/static/js/14.4da9b599.chunk.js"
+  },
+  {
+    "revision": "0cc59b9c30c1f61ff1aa",
+    "url": "/GrandisLibrary/static/js/15.2a0ed4b0.chunk.js"
+  },
+  {
+    "revision": "eefb9762ea5a3cb11435",
+    "url": "/GrandisLibrary/static/js/16.882939ff.chunk.js"
+  },
+  {
+    "revision": "2429ab92c89e2870cbb6",
+    "url": "/GrandisLibrary/static/js/17.c20d9e7b.chunk.js"
+  },
+  {
+    "revision": "4769085f5a1e28c38ba0",
+    "url": "/GrandisLibrary/static/js/4.59efbd9d.chunk.js"
+  },
+  {
+    "revision": "f5e1d530f997e482a8c4c7c43876c9cd",
+    "url": "/GrandisLibrary/static/js/4.59efbd9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "898f87e880a84d3c56b3",
+    "url": "/GrandisLibrary/static/js/5.47249eb0.chunk.js"
+  },
+  {
+    "revision": "ea10db75c85af0487a0f",
+    "url": "/GrandisLibrary/static/js/6.6e10e2f6.chunk.js"
+  },
+  {
+    "revision": "95c8c90e7e69b797020e",
+    "url": "/GrandisLibrary/static/js/7.62c4cfe3.chunk.js"
+  },
+  {
+    "revision": "db3791171e34203bf235",
+    "url": "/GrandisLibrary/static/js/8.37ff0185.chunk.js"
+  },
+  {
+    "revision": "47016e92f49cd7f3efb5",
+    "url": "/GrandisLibrary/static/js/9.7d09260e.chunk.js"
+  },
+  {
+    "revision": "42317971cba03d7c99fb",
+    "url": "/GrandisLibrary/static/js/main.d0135aa3.chunk.js"
+  },
+  {
+    "revision": "4f6f45cebb4edd44d330",
+    "url": "/GrandisLibrary/static/js/runtime-main.ed137f00.js"
+  },
+  {
+    "revision": "5f73f38f168011d3a2c425fde00d848e",
+    "url": "/GrandisLibrary/static/media/loading.5f73f38f.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/GrandisLibrary/precache-manifest.4623f972261c141676a277bf4a25b947.js"
+  "/GrandisLibrary/precache-manifest.2c7975a5ff0b77b360e1c300ac142a08.js"
 );
 
 self.addEventListener('message', (event) => {
